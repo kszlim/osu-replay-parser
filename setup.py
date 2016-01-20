ad4b0a7ad1c43e59161a743a27e098b4c1017924
Kevin Lim <kszlim@gmail.com>
2016-01-20 14:40:48 -0800

    update python supported versions

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ from setuptools import find_packages
 
 setup(
     name = 'osrparse',
-    version = '1.0.1',
+    version = '1.0.2',
     description = "Python implementation of osu! rhythm game replay parser.",
     classifiers = [
         'Topic :: Games/Entertainment',
         'Topic :: Software Development :: Libraries',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: Python'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
     ],
     keywords = ['osu!, osr, replay, replays, parsing, parser, python'],
     author = 'Kevin Lim',
     author_email = 'kszlim@gmail.com',
     url = 'https://github.com/kszlim/osu-replay-parser',
-    download_url = 'https://github.com/kszlim/osu-replay-parser/tarball/1.0.1',
+    download_url = 'https://github.com/kszlim/osu-replay-parser/tarball/1.0.2',
     license = 'MIT',
     test_suite="tests",
     packages = find_packages()
