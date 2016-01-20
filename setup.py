6037398fdf1e94fe946a3dbf0ce01fabb5f0f60e
Kevin Lim <kszlim@gmail.com>
2016-01-20 01:07:37 -0800

    update setup

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ import setuptools
 version = '0.1.0'
 
 setuptools.setup(
-    name='osrparse',
-    version=version,
-    description="Python implementation of osu! rhythm game replays.",
-    classifiers=[
+    name = 'osrparse',
+    version = version,
+    description = "Python implementation of osu! rhythm game replays.",
+    classifiers = [
         'Topic :: Games/Entertainment',
         'Topic :: Software Development :: Libraries',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python'
     ],
-    keywords='osu!, osr, replay, replays, parsing, parser, python',
-    author='Kevin Lim',
-    author_email='kszlim@gmail.com',
-    url='https://github.com/',
-    license='MIT',
-    packages=['osrparse']
+    keywords = ['osu!, osr, replay, replays, parsing, parser, python'],
+    author = 'Kevin Lim',
+    author_email = 'kszlim@gmail.com',
+    url = 'https://github.com/kszlim/osu-replay-parser',
+    download_url = 'https://github.com/kszlim/osu-replay-parser/tarball/0.1'
+    license = 'MIT',
+    packages = ['osrparse']
 )
