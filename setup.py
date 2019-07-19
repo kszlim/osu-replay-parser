d9676b22f82c0a435fbfcc68ee8bd64e16675984
tybug <orionldevoe@gmail.com>
2019-07-19 18:08:42 -0400

    version bump, various info update

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-version = '4.1.0'
+version = "5.0.0"
+
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
-    name = 'osrparse',
+    name = "circleparse",
     version = version,
-    description = "Python implementation of osu! rhythm game replay parser.",
+    description = "Parser for osr files and lzma replay streams for osu!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers = [
-        'Topic :: Games/Entertainment',
-        'Topic :: Software Development :: Libraries',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        "Topic :: Games/Entertainment",
+        "Topic :: Software Development :: Libraries",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"
     ],
-    keywords = ['osu!, osr, replay, replays, parsing, parser, python'],
-    author = 'Liam DeVoe',
-    author_email = 'orionldevoe@gmail.com',
-    url = 'https://github.com/circleguard/osu-replay-parser',
-    download_url = 'https://github.com/circleguard/osu-replay-parser/tarball/' + version,
-    license = 'MIT',
+    keywords = ["osu!, osr, replay, replays, parsing, parser, python"],
+    author = "Liam DeVoe",
+    author_email = "orionldevoe@gmail.com",
+    url = "https://github.com/circleguard/circleparse",
+    download_url = "https://github.com/circleguard/circleparse/tarball/" + version,
+    license = "MIT",
     test_suite="tests",
     packages = find_packages()
 )
