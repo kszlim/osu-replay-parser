c5d474e30f2c56647bb77d5ec76b249125913549
Liam DeVoe <orionldevoe@gmail.com>
2019-12-08 15:13:50 -0500

    proper enum casing

--- circleparse/enums.py
+++ circleparse/enums.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class GameMode(Enum):
-    Standard = 0
-    Taiko = 1
-    CatchTheBeat = 2
-    Osumania = 3
+    STD    = 0
+    TAIKO  = 1
+    CTB    = 2
+    MANIA  = 3
