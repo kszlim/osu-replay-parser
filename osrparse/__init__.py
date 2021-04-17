868f18bf1e0bee7f7516a285c4011597d61f25b4
Liam DeVoe <orionldevoe@gmail.com>
2021-04-16 22:20:56 -0400

    bump version to 4.0.1

--- osrparse/__init__.py
+++ osrparse/__init__.py
@@ -2,7 +2,7 @@ from osrparse.enums import GameMode, Mod
 from osrparse.parse import parse_replay_file, parse_replay
 from osrparse.replay import Replay, ReplayEvent
 
-__version__ = "4.0.0"
+__version__ = "4.0.1"
 
 
 __all__ = ["GameMode", "Mod", "parse_replay_file", "parse_replay", "Replay",
