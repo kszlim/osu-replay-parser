452c78aa6e72c1a3f5832961d3d0c5d0352d6f8a
Liam DeVoe <orionldevoe@gmail.com>
2021-05-25 13:21:31 -0400

    export KeyTaiko and KeyMania

--- osrparse/__init__.py
+++ osrparse/__init__.py
@@ -1,5 +1,5 @@
 from osrparse.utils import (GameMode, Mod, Key, ReplayEvent, ReplayEventOsu,
-    ReplayEventTaiko, ReplayEventMania, ReplayEventCatch)
+    ReplayEventTaiko, ReplayEventMania, ReplayEventCatch, KeyTaiko, KeyMania)
 from osrparse.parse import parse_replay_file, parse_replay
 from osrparse.replay import Replay
 
@@ -8,4 +8,4 @@ __version__ = "4.0.1"
 
 __all__ = ["GameMode", "Mod", "parse_replay_file", "parse_replay", "Replay",
     "ReplayEvent", "Key", "ReplayEventOsu", "ReplayEventTaiko",
-    "ReplayEventMania", "ReplayEventCatch"]
+    "ReplayEventMania", "ReplayEventCatch", "KeyTaiko", "KeyMania"]
