d38b5fc12ba7c5113b10ea7f39f01d6b2d2a479f
Liam DeVoe <orionldevoe@gmail.com>
2022-12-11 13:13:56 -0500

    bump version to 6.0.2

--- osrparse/__init__.py
+++ osrparse/__init__.py
@@ -2,7 +2,7 @@ from osrparse.utils import (GameMode, Mod, Key, ReplayEvent, ReplayEventOsu,
     ReplayEventTaiko, ReplayEventMania, ReplayEventCatch, KeyTaiko, KeyMania)
 from osrparse.replay import Replay, parse_replay_data
 
-__version__ = "6.0.1"
+__version__ = "6.0.2"
 
 
 __all__ = ["GameMode", "Mod", "Replay", "ReplayEvent", "Key",
