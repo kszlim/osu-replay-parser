3503c147cd972e6714feaf85e088b5fab96fda0f
Liam DeVoe <orionldevoe@gmail.com>
2023-05-10 19:13:39 -0400

    move to pyproject.toml for building

--- osrparse/__init__.py
+++ osrparse/__init__.py
@@ -1,9 +1,10 @@
+from importlib import metadata
+
 from osrparse.utils import (GameMode, Mod, Key, ReplayEvent, ReplayEventOsu,
     ReplayEventTaiko, ReplayEventMania, ReplayEventCatch, KeyTaiko, KeyMania)
 from osrparse.replay import Replay, parse_replay_data
 
-__version__ = "6.0.2"
-
+__version__ = metadata.version(__package__)
 
 __all__ = ["GameMode", "Mod", "Replay", "ReplayEvent", "Key",
     "ReplayEventOsu", "ReplayEventTaiko", "ReplayEventMania",
